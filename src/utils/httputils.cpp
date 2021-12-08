599f3bb4a46a0413ccae7ed41c14b99136386112
Rosen Penev <rosenp@gmail.com>
2021-12-07 17:15:21 -0800

    fix compilation with UPNP_ENABLE_UNSPECIFIED_SERVER
    Missing ;
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- src/utils/httputils.cpp
+++ src/utils/httputils.cpp
@@ -376,7 +376,7 @@ static const std::string& get_sdk_common_info()
     if (sdk_common_info.empty()) {
         std::ostringstream ostr;
 #ifdef UPNP_ENABLE_UNSPECIFIED_SERVER
-        ostr << "Unspecified UPnP/1.0 Unspecified"
+        ostr << "Unspecified UPnP/1.0 Unspecified";
 #else /* UPNP_ENABLE_UNSPECIFIED_SERVER */
 #ifdef _WIN32
             OSVERSIONINFO versioninfo;
