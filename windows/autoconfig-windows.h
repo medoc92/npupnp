2eca6d514294a5c0a475d634eebe0e575368ae3e
Jean-Francois Dockes <jf@dockes.org>
2023-12-31 09:25:32 +0100

    version bump

--- windows/autoconfig-windows.h
+++ windows/autoconfig-windows.h
@@ -103,7 +103,7 @@
 #define PACKAGE_NAME "libnpupnp"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libnpupnp 6.0.4"
+#define PACKAGE_STRING "libnpupnp 6.0.5"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "libnpupnp"
@@ -112,7 +112,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "6.0.4"
+#define PACKAGE_VERSION "6.0.5"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
